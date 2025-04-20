c1dce82744157584b6fe2b2caf8309096c3beef8
Mupaser <sergioparejo18@gmail.com>
2025-04-20 17:01:01 +0200

    Modelos y migraciones

---
+++ database/migrations/2025_10_13_160931_create_order_lines_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->double('totalPrice');
+            $table->foreignId('order_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('piece_id')->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_lines');
+    }
+};
