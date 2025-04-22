b4e1e6f7322e69acfc899a8e9dd18d18b795869b
jagutinu <jagutinu@alu.upo.es>
2025-04-22 13:14:09 +0200

    Controladoder rol y orderline

---
+++ app/Http/Controllers/PieceController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Piece;
+use Illuminate\Http\Request;
+
+class PieceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pieces = Piece::paginate(10);
+        return view('', compact('pieces'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $piece = new Piece();
+        $piece->name = $request->name;
+        $piece->price = $request->price;
+        $piece->estate = $request->estate;
+        $piece->offer = $request->offer;
+        $piece->description = $request->description;
+        $piece->image = $request->image;
+        $piece->save();
+        return view('', compact('piece'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Piece $piece)
+    {
+        return view('', compact('piece'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Piece $piece)
+    {
+        return view('', compact('piece'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Piece $piece)
+    {
+        $piece->name = $request->name;
+        $piece->price = $request->price;
+        $piece->estate = $request->estate;
+        $piece->offer = $request->offer;
+        $piece->description = $request->description;
+        $piece->image = $request->image;
+        $piece->save();
+        return view('', compact('piece'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Piece $piece)
+    {
+        $piece->delete();
+        return view();
+    }
+}
